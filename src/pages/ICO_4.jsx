78cf3be7e7bbe4921f000bd0879a357802fed6b3
Mavrick <vivekkhudaniya@gmail.com>
2024-12-06 14:32:28 +0530

    commented code

--- src/pages/ICO_4.jsx
+++ src/pages/ICO_4.jsx
@@ -1,92 +1,92 @@
-import { FunctionComponent, useCallback } from 'react';
-import styles from '../css_modules/ICO_4.module.css';
-import {images} from '../StoreImages/StoreImages';
-import ConnectButton from "@/components/shared/ConnectButton"
-import { useRouter } from 'next/navigation';
+// import { FunctionComponent, useCallback } from 'react';
+// import styles from '../css_modules/ICO_4.module.css';
+// import {images} from '../StoreImages/StoreImages';
+// import ConnectButton from "@/components/shared/ConnectButton"
+// import { useRouter } from 'next/navigation';
 
 
-const ICO_4 = () => {
-    const router = useRouter();
+// const ICO_4 = () => {
+//     const router = useRouter();
   	
-  	return (
-    		<div className={styles.ico4}>
-      			<div className={styles.profilerectangle} />
-      			<div className={styles.goalrectangle} />
-      			<div className={styles.goalrectangle1} />
-      			<div className={styles.goalrectangle2} />
-      			<div className={styles.goalrectangle3} />
+//   	return (
+//     		<div className={styles.ico4}>
+//       			<div className={styles.profilerectangle} />
+//       			<div className={styles.goalrectangle} />
+//       			<div className={styles.goalrectangle1} />
+//       			<div className={styles.goalrectangle2} />
+//       			<div className={styles.goalrectangle3} />
       		
-      			<img className={styles.userlogoIcon} alt="" src={images.UserLogo} />
-      			<b className={styles.wxyz}>WXYZ</b>
+//       			<img className={styles.userlogoIcon} alt="" src={images.UserLogo} />
+//       			<b className={styles.wxyz}>WXYZ</b>
       		
-      			<img className={styles.useropenicon} alt="" src={images.UserOpenIcon}/>
-				  <div className={styles.walletButton}>
-        <ConnectButton />
-      </div>
-      			<div className={styles.goalNotSet}>GOAL: NOT SET</div>
-      			<div className={styles.goal50000}>GOAL: $50,000</div>
-      			<div className={styles.goal20000}>GOAL: $20,000</div>
-      			<div className={styles.goal25000}>GOAL: $25,000</div>
-      			<div className={styles.bovinverse}>BovinVerse</div>
-      			<div className={styles.chirplay}>Chirplay</div>
-      			<div className={styles.metacloud}>MetaCloud</div>
-      			<div className={styles.inshape}>inSHAPE</div>
-      			<div className={styles.plateform}>Plateform</div>
-      			<div className={styles.marketplace}>Marketplace</div>
-      			<div className={styles.vr}>VR</div>
-      			<div className={styles.blockchainService}>Blockchain Service</div>
-      			<div className={styles.usdt}>7,000 / 20,000 USDT</div>
-      			<div className={styles.usdt1}>4,000 / 20,000 USDT</div>
-      			<div className={styles.usdt2}>9,000 / 20,000 USDT</div>
-      			<div className={styles.usdt3}>4,000 / 20,000 USDT</div>
-      			<div className={styles.progress}>30% PROGRESS</div>
-      			<div className={styles.progress1}>10% PROGRESS</div>
-      			<div className={styles.progress2}>45% PROGRESS</div>
-      			<div className={styles.progress3}>45% PROGRESS</div>
-      			<div className={styles.notRated}>Not Rated</div>
-      			<div className={styles.notRated1}>Not Rated</div>
-      			<div className={styles.notRated2}>Not Rated</div>
-      			<div className={styles.notRated3}>Not Rated</div>
-      			<div className={styles.dec}>1 Dec</div>
-      			<div className={styles.dec1}>8 Dec</div>
-      			<div className={styles.dec2}>11 Dec</div>
-      			<div className={styles.dec3}>19 Dec</div>
-      			<div className={styles.bovinverseimageborder} />
-      			<div className={styles.chirplayimageborder} />
-      			<div className={styles.metacloudimageborder} />
-      			<div className={styles.inshapeimageborder} />
-      			<b className={styles.b}>.........................................................................</b>
-      			<b className={styles.b1}>.........................................................................</b>
-      			<b className={styles.b2}>.........................................................................</b>
-      			<img className={styles.buttonsBackground} alt="" src={images.ButtonsBackground}  />
-      			<img className={styles.homeButtonIcon} alt="" src={images.HomeButton} onClick={onHomeButtonIconClick} />
-      			<img className={styles.notificationButtonIcon} alt="" src={images.NotificationButton} />
-      			<img className={styles.stackButtonIcon} alt="" src={images.StackButton} />
-      			<img className={styles.cursorbuttonsIcon} alt="" src={images.CursorButtons} />
-      			<img className={styles.icoButtonIcon} alt="" src={images.ICOButton} />
-      			<img className={styles.nftButtonIcon} alt="" src={images.NFTButton} onClick={onHomeButtonIconClick} />
-      			<div className={styles.bovinversedark} />
-      			<div className={styles.chirplaydark} />
-      			<div className={styles.metaclouddark} />
-      			<div className={styles.inshapedark} />
-      			<div className={styles.bovinverse1} />
-      			<div className={styles.chirplay1} />
-      			<div className={styles.metacloud1} />
-      			<div className={styles.inshape1} />
-      			<img className={styles.metacloudimageIcon} alt="" src={images.MetaCloudImage} />
-      			<img className={styles.inshapeimageIcon} alt="" src={images.inShapeImage} />
-      			{/* <img className={styles.dropdownico4Icon} alt="" src="DropDownIco_4.svg" /> */}
-      			<img className={styles.auerectangle} alt="" src={images.AUERectangle}  />
-      			<div className={styles.upcomingrectangle} />
-      			<b className={styles.active}>{`ACTIVE `}</b>
-      			<b className={styles.upcoming}>UPCOMING</b>
-      			<b className={styles.ended} onClick={onHomeButtonIconClick}>ENDED</b>
-      			<img className={styles.upcomingicon} alt="" src={images.UpcomingIcon} />
-      			<div className={styles.auerectangleborder} />
-      			<div className={styles.auerectanglestatus} />
-      			<img className={styles.bovinverseimageIcon} alt="" src={images.BovinVerseImage} />
-      			<img className={styles.chirplayimageIcon} alt="" src={images.ChirplayImage} />
-    		</div>);
-};
+//       			<img className={styles.useropenicon} alt="" src={images.UserOpenIcon}/>
+// 				  <div className={styles.walletButton}>
+//         <ConnectButton />
+//       </div>
+//       			<div className={styles.goalNotSet}>GOAL: NOT SET</div>
+//       			<div className={styles.goal50000}>GOAL: $50,000</div>
+//       			<div className={styles.goal20000}>GOAL: $20,000</div>
+//       			<div className={styles.goal25000}>GOAL: $25,000</div>
+//       			<div className={styles.bovinverse}>BovinVerse</div>
+//       			<div className={styles.chirplay}>Chirplay</div>
+//       			<div className={styles.metacloud}>MetaCloud</div>
+//       			<div className={styles.inshape}>inSHAPE</div>
+//       			<div className={styles.plateform}>Plateform</div>
+//       			<div className={styles.marketplace}>Marketplace</div>
+//       			<div className={styles.vr}>VR</div>
+//       			<div className={styles.blockchainService}>Blockchain Service</div>
+//       			<div className={styles.usdt}>7,000 / 20,000 USDT</div>
+//       			<div className={styles.usdt1}>4,000 / 20,000 USDT</div>
+//       			<div className={styles.usdt2}>9,000 / 20,000 USDT</div>
+//       			<div className={styles.usdt3}>4,000 / 20,000 USDT</div>
+//       			<div className={styles.progress}>30% PROGRESS</div>
+//       			<div className={styles.progress1}>10% PROGRESS</div>
+//       			<div className={styles.progress2}>45% PROGRESS</div>
+//       			<div className={styles.progress3}>45% PROGRESS</div>
+//       			<div className={styles.notRated}>Not Rated</div>
+//       			<div className={styles.notRated1}>Not Rated</div>
+//       			<div className={styles.notRated2}>Not Rated</div>
+//       			<div className={styles.notRated3}>Not Rated</div>
+//       			<div className={styles.dec}>1 Dec</div>
+//       			<div className={styles.dec1}>8 Dec</div>
+//       			<div className={styles.dec2}>11 Dec</div>
+//       			<div className={styles.dec3}>19 Dec</div>
+//       			<div className={styles.bovinverseimageborder} />
+//       			<div className={styles.chirplayimageborder} />
+//       			<div className={styles.metacloudimageborder} />
+//       			<div className={styles.inshapeimageborder} />
+//       			<b className={styles.b}>.........................................................................</b>
+//       			<b className={styles.b1}>.........................................................................</b>
+//       			<b className={styles.b2}>.........................................................................</b>
+//       			<img className={styles.buttonsBackground} alt="" src={images.ButtonsBackground}  />
+//       			<img className={styles.homeButtonIcon} alt="" src={images.HomeButton} onClick={onHomeButtonIconClick} />
+//       			<img className={styles.notificationButtonIcon} alt="" src={images.NotificationButton} />
+//       			<img className={styles.stackButtonIcon} alt="" src={images.StackButton} />
+//       			<img className={styles.cursorbuttonsIcon} alt="" src={images.CursorButtons} />
+//       			<img className={styles.icoButtonIcon} alt="" src={images.ICOButton} />
+//       			<img className={styles.nftButtonIcon} alt="" src={images.NFTButton} onClick={onHomeButtonIconClick} />
+//       			<div className={styles.bovinversedark} />
+//       			<div className={styles.chirplaydark} />
+//       			<div className={styles.metaclouddark} />
+//       			<div className={styles.inshapedark} />
+//       			<div className={styles.bovinverse1} />
+//       			<div className={styles.chirplay1} />
+//       			<div className={styles.metacloud1} />
+//       			<div className={styles.inshape1} />
+//       			<img className={styles.metacloudimageIcon} alt="" src={images.MetaCloudImage} />
+//       			<img className={styles.inshapeimageIcon} alt="" src={images.inShapeImage} />
+//       			{/* <img className={styles.dropdownico4Icon} alt="" src="DropDownIco_4.svg" /> */}
+//       			<img className={styles.auerectangle} alt="" src={images.AUERectangle}  />
+//       			<div className={styles.upcomingrectangle} />
+//       			<b className={styles.active}>{`ACTIVE `}</b>
+//       			<b className={styles.upcoming}>UPCOMING</b>
+//       			<b className={styles.ended} onClick={onHomeButtonIconClick}>ENDED</b>
+//       			<img className={styles.upcomingicon} alt="" src={images.UpcomingIcon} />
+//       			<div className={styles.auerectangleborder} />
+//       			<div className={styles.auerectanglestatus} />
+//       			<img className={styles.bovinverseimageIcon} alt="" src={images.BovinVerseImage} />
+//       			<img className={styles.chirplayimageIcon} alt="" src={images.ChirplayImage} />
+//     		</div>);
+// };
 
-export default ICO_4;
+// export default ICO_4;
--- src/pages/ICO_5.jsx
+++ src/pages/ICO_5.jsx
@@ -1,99 +1,99 @@
-import { FunctionComponent, useCallback } from 'react';
-import styles from '../css_modules/ICO_5.module.css';
-import {images} from '../StoreImages/StoreImages';
-import ConnectButton from "@/components/shared/ConnectButton"
-import { useRouter } from 'next/navigation';
+// import { FunctionComponent, useCallback } from 'react';
+// import styles from '../css_modules/ICO_5.module.css';
+// import {images} from '../StoreImages/StoreImages';
+// import ConnectButton from "@/components/shared/ConnectButton"
+// import { useRouter } from 'next/navigation';
 
 
-const ICO_5 = () => {
-    const router = useRouter();
+// const ICO_5 = () => {
+//     const router = useRouter();
   	
-  	const onACTIVETextClick = useCallback(() => {
-    		// Add your code here
-  	}, []);
+//   	const onACTIVETextClick = useCallback(() => {
+//     		// Add your code here
+//   	}, []);
   	
-  	return (
-    		<div className={styles.ico5}>
-      			<div className={styles.profilerectangle} />
-      			<div className={styles.inshapepricerectangle} />
-      			<div className={styles.arrakispricerectangle} />
-      			<div className={styles.metacloudpricerectangle} />
-      			<div className={styles.inshapepricerectangle1} />
+//   	return (
+//     		<div className={styles.ico5}>
+//       			<div className={styles.profilerectangle} />
+//       			<div className={styles.inshapepricerectangle} />
+//       			<div className={styles.arrakispricerectangle} />
+//       			<div className={styles.metacloudpricerectangle} />
+//       			<div className={styles.inshapepricerectangle1} />
       			
-      			<img className={styles.userlogoIcon} alt="" src={images.UserLogo} />
-      			<b className={styles.wxyz}>WXYZ</b>
+//       			<img className={styles.userlogoIcon} alt="" src={images.UserLogo} />
+//       			<b className={styles.wxyz}>WXYZ</b>
       			
-      			<img className={styles.useropenicon} alt="" src={images.UserOpenIcon}/>
-				  <div className={styles.walletButton}>
-        <ConnectButton />
-      </div>
+//       			<img className={styles.useropenicon} alt="" src={images.UserOpenIcon}/>
+// 				  <div className={styles.walletButton}>
+//         <ConnectButton />
+//       </div>
 
-				<img className={styles.auerectangle} alt="" src={images.AUERectangle}  />
+// 				<img className={styles.auerectangle} alt="" src={images.AUERectangle}  />
 
 
-      			<div className={styles.endedrectangle} />
-      			<b className={styles.active} onClick={onACTIVETextClick}>{`ACTIVE `}</b>
-      			<b className={styles.upcoming} onClick={onACTIVETextClick}>UPCOMING</b>
-      			<b className={styles.ended}>ENDED</b>
-      			<img className={styles.endedicon} alt="" src={images.EndedIcon} />
-      			<div className={styles.auerectangleborder} />
-      			<div className={styles.auerectanglestatus} />
-      			<div className={styles.usdt}>$1,280 USDT</div>
-      			<div className={styles.usdt1}>$1,000 USDT</div>
-      			<div className={styles.usdt2}>$900 USDT</div>
-      			<div className={styles.usdt3}>$900 USDT</div>
-      			<div className={styles.inshape}>inSHAPE</div>
-      			<div className={styles.arrakis}>Arrakis</div>
-      			<div className={styles.metacloud}>MetaCloud</div>
-      			<div className={styles.inshape1}>inSHAPE</div>
-      			<div className={styles.blockchainService}>Blockchain Service</div>
-      			<div className={styles.protocol}>Protocol</div>
-      			<div className={styles.vr}>VR</div>
-      			<div className={styles.blockchainService1}>Blockchain Service</div>
-      			<div className={styles.usdt4}>7,000 / 20,000 USDT</div>
-      			<div className={styles.usdt5}>4,000 / 20,000 USDT</div>
-      			<div className={styles.usdt6}>9,000 / 20,000 USDT</div>
-      			<div className={styles.usdt7}>4,000 / 20,000 USDT</div>
-      			<div className={styles.progress}>30% PROGRESS</div>
-      			<div className={styles.progress1}>10% PROGRESS</div>
-      			<div className={styles.progress2}>45% PROGRESS</div>
-      			<div className={styles.progress3}>45% PROGRESS</div>
-      			<div className={styles.notRated}>Not Rated</div>
-      			<div className={styles.notRated1}>Not Rated</div>
-      			<div className={styles.notRated2}>Not Rated</div>
-      			<div className={styles.notRated3}>Not Rated</div>
-      			<div className={styles.ended15Dec}>Ended: 15 Dec</div>
-      			<div className={styles.ended19Dec}>Ended: 19 Dec</div>
-      			<div className={styles.ended25Dec}>Ended: 25 Dec</div>
-      			<div className={styles.ended30Dec}>Ended: 30 Dec</div>
-      			<div className={styles.inshapeimageborder} />
-      			<div className={styles.arrakisimageborder} />
-      			<div className={styles.metacloudimageborder} />
-      			<div className={styles.inshapeimageborder1} />
-      			<b className={styles.b}>.........................................................................</b>
-      			<b className={styles.b1}>.........................................................................</b>
-      			<b className={styles.b2}>.........................................................................</b>
-      			<img className={styles.inshapeimageIcon} alt="" src={images.inShapeImage} />
-      			<img className={styles.buttonsBackground} alt="" src={images.ButtonsBackground}  />
-      			<img className={styles.homeButtonIcon} alt="" src={images.HomeButton} onClick={onACTIVETextClick} />
-      			<img className={styles.notificationButtonIcon} alt="" src={images.NotificationButton} />
-      			<img className={styles.stackButtonIcon} alt="" src={images.StackButton} />
-      			<img className={styles.cursorbuttonsIcon} alt="" src={images.CursorButtons} />
-      			<img className={styles.icoButtonIcon} alt="" src={images.ICOButton} />
-      			<img className={styles.nftButtonIcon} alt="" src={images.NFTButton} onClick={onACTIVETextClick} />
-      			<div className={styles.inshapedark} />
-      			<div className={styles.arrakisdark} />
-      			<div className={styles.metaclouddark} />
-      			<div className={styles.inshapedark1} />
-      			<div className={styles.inshape2} />
-      			<div className={styles.arrakis1} />
-      			<div className={styles.metacloud1} />
-      			<div className={styles.inshape3} />
-      			<img className={styles.arrakisimageIcon} alt="" src={images.ArrakisImage} />
-      			<img className={styles.metacloudimageIcon} alt="" src={images.MetaCloudImage} />
-      			<img className={styles.inshapeimageIcon1} alt="" src={images.inShapeImage} />
-      			<img className={styles.dropdownico5Icon} alt="" src={images.DownNotification} />
-    		</div>);
-};
+//       			<div className={styles.endedrectangle} />
+//       			<b className={styles.active} onClick={onACTIVETextClick}>{`ACTIVE `}</b>
+//       			<b className={styles.upcoming} onClick={onACTIVETextClick}>UPCOMING</b>
+//       			<b className={styles.ended}>ENDED</b>
+//       			<img className={styles.endedicon} alt="" src={images.EndedIcon} />
+//       			<div className={styles.auerectangleborder} />
+//       			<div className={styles.auerectanglestatus} />
+//       			<div className={styles.usdt}>$1,280 USDT</div>
+//       			<div className={styles.usdt1}>$1,000 USDT</div>
+//       			<div className={styles.usdt2}>$900 USDT</div>
+//       			<div className={styles.usdt3}>$900 USDT</div>
+//       			<div className={styles.inshape}>inSHAPE</div>
+//       			<div className={styles.arrakis}>Arrakis</div>
+//       			<div className={styles.metacloud}>MetaCloud</div>
+//       			<div className={styles.inshape1}>inSHAPE</div>
+//       			<div className={styles.blockchainService}>Blockchain Service</div>
+//       			<div className={styles.protocol}>Protocol</div>
+//       			<div className={styles.vr}>VR</div>
+//       			<div className={styles.blockchainService1}>Blockchain Service</div>
+//       			<div className={styles.usdt4}>7,000 / 20,000 USDT</div>
+//       			<div className={styles.usdt5}>4,000 / 20,000 USDT</div>
+//       			<div className={styles.usdt6}>9,000 / 20,000 USDT</div>
+//       			<div className={styles.usdt7}>4,000 / 20,000 USDT</div>
+//       			<div className={styles.progress}>30% PROGRESS</div>
+//       			<div className={styles.progress1}>10% PROGRESS</div>
+//       			<div className={styles.progress2}>45% PROGRESS</div>
+//       			<div className={styles.progress3}>45% PROGRESS</div>
+//       			<div className={styles.notRated}>Not Rated</div>
+//       			<div className={styles.notRated1}>Not Rated</div>
+//       			<div className={styles.notRated2}>Not Rated</div>
+//       			<div className={styles.notRated3}>Not Rated</div>
+//       			<div className={styles.ended15Dec}>Ended: 15 Dec</div>
+//       			<div className={styles.ended19Dec}>Ended: 19 Dec</div>
+//       			<div className={styles.ended25Dec}>Ended: 25 Dec</div>
+//       			<div className={styles.ended30Dec}>Ended: 30 Dec</div>
+//       			<div className={styles.inshapeimageborder} />
+//       			<div className={styles.arrakisimageborder} />
+//       			<div className={styles.metacloudimageborder} />
+//       			<div className={styles.inshapeimageborder1} />
+//       			<b className={styles.b}>.........................................................................</b>
+//       			<b className={styles.b1}>.........................................................................</b>
+//       			<b className={styles.b2}>.........................................................................</b>
+//       			<img className={styles.inshapeimageIcon} alt="" src={images.inShapeImage} />
+//       			<img className={styles.buttonsBackground} alt="" src={images.ButtonsBackground}  />
+//       			<img className={styles.homeButtonIcon} alt="" src={images.HomeButton} onClick={onACTIVETextClick} />
+//       			<img className={styles.notificationButtonIcon} alt="" src={images.NotificationButton} />
+//       			<img className={styles.stackButtonIcon} alt="" src={images.StackButton} />
+//       			<img className={styles.cursorbuttonsIcon} alt="" src={images.CursorButtons} />
+//       			<img className={styles.icoButtonIcon} alt="" src={images.ICOButton} />
+//       			<img className={styles.nftButtonIcon} alt="" src={images.NFTButton} onClick={onACTIVETextClick} />
+//       			<div className={styles.inshapedark} />
+//       			<div className={styles.arrakisdark} />
+//       			<div className={styles.metaclouddark} />
+//       			<div className={styles.inshapedark1} />
+//       			<div className={styles.inshape2} />
+//       			<div className={styles.arrakis1} />
+//       			<div className={styles.metacloud1} />
+//       			<div className={styles.inshape3} />
+//       			<img className={styles.arrakisimageIcon} alt="" src={images.ArrakisImage} />
+//       			<img className={styles.metacloudimageIcon} alt="" src={images.MetaCloudImage} />
+//       			<img className={styles.inshapeimageIcon1} alt="" src={images.inShapeImage} />
+//       			<img className={styles.dropdownico5Icon} alt="" src={images.DownNotification} />
+//     		</div>);
+// };
 
-export default ICO_5;
+// export default ICO_5;
